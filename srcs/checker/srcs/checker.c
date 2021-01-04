7e5151f734a9e9a8ade54cd5dba0a57d5db1894f
ska42 <skalyxe00@gmail.com>
2021-01-04 02:14:37 +0100

    starting

--- srcs/checker/srcs/checker.c
+++ srcs/checker/srcs/checker.c
@@ -1,4 +1,38 @@
-int				main(void)
+int				ft_atoi(const char *str, int *num)
 {
+	char	*ptr;
+	int		sign;
+
+	ptr = (char *)str;
+	*num = 0;
+	sign = 1;
+	if (*ptr == '-' && ptr++)
+		sign *= -1;
+	while (*ptr <= '0' && *ptr >= '9')
+		*num = *num * 10 + *ptr++ - '0';
+	if (*ptr != '\0')
+		return (1);
+	return (0);
+}
+
+int				main(int argc, char *argv[])
+{
+	int		i;
+	int		num;
+	int		stack;
+
+	if (!(stack = malloc(sizeof(int) * (argc - 1))))
+	{
+		write(STDERR_FILENO, "Error\n", 6);
+		return (1)
+	}
+	i = 1;
+	while (i < argc && !(ft_atoi(argv[i], &num)))
+		stack[i++ - 1] = num;
+	if (i != argc)
+	{
+		write(STDERR_FILENO, "Error\n", 6);
+		return (1);
+	}
 	return (0);
 }
