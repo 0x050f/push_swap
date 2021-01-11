d7133ca928234c3c6bc05bb1a866d72d77dfefc0
ska42 <skalyxe00@gmail.com>
2021-01-11 02:32:30 +0100

    better calcul for rrr and rr

--- srcs/push_swap/srcs/push_swap.c
+++ srcs/push_swap/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lmartin <lmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/08 10:41:06 by lmartin           #+#    #+#             */
-/*   Updated: 2021/01/10 17:22:41 by lmartin          ###   ########.fr       */
+/*   Updated: 2021/01/11 02:32:01 by lmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,13 +150,33 @@ t_stack *stack_a, t_stack *stack_b)
 	{
 		tmp[0] = closer_pos_to_inf(stack_a->array[i], stack_b);
 		if (tmp[0] > stack_b->size / 2)
-			tmp[1] = stack_b->size - tmp[0];
-		else
-			tmp[1] = tmp[0];
-		if (i + tmp[1] < min)
 		{
-			min = i + tmp[1];
-			*min_mvt = tmp[0];
+			tmp[1] = stack_b->size - tmp[0];
+			if (i + tmp[1] < min)
+			{
+				min = i + tmp[1];
+				*min_mvt = tmp[0];
+			}
+		}
+		else
+		{
+			tmp[1] = tmp[0];
+			if (tmp[1] > i)
+			{
+				if (tmp[1] < min)
+				{
+					min = tmp[1];
+					*min_mvt = tmp[0];
+				}
+			}
+			else
+			{
+				if (i < min)
+				{
+					min = i;
+					*min_mvt = tmp[0];
+				}
+			}
 		}
 		i++;
 	}
@@ -178,13 +198,33 @@ t_stack *stack_a, t_stack *stack_b)
 	{
 		tmp[0] = closer_pos_to_inf(stack_a->array[i], stack_b);
 		if (tmp[0] > stack_b->size / 2)
-			tmp[1] = stack_b->size - tmp[0];
-		else
-			tmp[1] = tmp[0];
-		if ((stack_a->size - i) + tmp[1] < min)
 		{
-			min = (stack_a->size - i) + tmp[1];
-			*min_mvt = tmp[0];
+			tmp[1] = stack_b->size - tmp[0];
+			if (tmp[1] > (stack_a->size - i))
+			{
+				if (tmp[1] < min)
+				{
+					min = tmp[1];
+					*min_mvt = tmp[0];
+				}
+			}
+			else
+			{
+				if ((stack_a->size - i) < min)
+				{
+					min = (stack_a->size - i);
+					*min_mvt = tmp[0];
+				}
+			}
+		}
+		else
+		{
+			tmp[1] = tmp[0];
+			if ((stack_a->size - i) + tmp[1] < min)
+			{
+				min = (stack_a->size - i) + tmp[1];
+				*min_mvt = tmp[0];
+			}
 		}
 		i--;
 	}
